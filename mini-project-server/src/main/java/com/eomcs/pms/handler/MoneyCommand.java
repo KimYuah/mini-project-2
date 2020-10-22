c6f51bc0a6d80635d908abfa6dd16bf951204198
eunchae-cho <dmsco8981@gmail.com>
2020-10-22 11:38:54 +0900

    ,

--- mini-project-server/src/main/java/com/eomcs/pms/handler/MoneyCommand.java
+++ mini-project-server/src/main/java/com/eomcs/pms/handler/MoneyCommand.java
@@ -9,16 +9,29 @@ public class MoneyCommand implements Command {
   @Override
   public void execute(PrintWriter out, BufferedReader in) {
     try {
-      out.println("\n[금전운]");
-
-      String input = Prompt.inputString("1 ~ 5번까지 끌리는 번호를 골라주세요!(예: 1) ", out, in);
+      out.println("\n[ 운세 보기 ]");
+      out.println("원하시는 운세 정보의 번호를 입력하세요.");
+      out.println(" 1. 성격운");
+      out.println(" 2. 취업운");
+      out.println(" 3. 학업운");
+      out.println(" 4. 금전운");
+      out.println(" 5. 연애운");
+      String input = Prompt.inputString("입력> ", out, in);
 
       switch (input) {
-        case "1": out.println("\n[1번의 금전운]"); out.println("지갑 잘 챙기세요! 금전운 50%"); break;
-        case "2": out.println("\n[2번의 금전운]"); out.println("우와!! 부러워요... 저 백원만 주세요!! 금전운 80%"); break;
-        case "3": out.println("\n[3번의 금전운]"); out.println("길가다가 돈 주울 수 있을지도...?!! 금전운 60%"); break;
-        case "4": out.println("\n[4번의 금전운]"); out.println("당장 로또를 사러가세요! 금전운 100%"); break;
-        case "5": out.println("\n[5번의 금전운]"); out.println("나쁘지않아요!! 금전운 70%"); break;
+        case "1": out.println("\n [ 성격운  ]");
+        	break;
+        case "2": 
+        	out.println("\n [ 취업운  ]");
+        	out.println("1 ~ 5번까지의 숫자중에 원하는 숫자 하나를 골라주세요:)");
+        	out.println("     1번    2번   3번   4번   5번 ");
+        	String choice = Prompt.inputString("입력> ", out, in);
+        	
+        	
+        	break;
+        case "3": out.println("\n 3번의 학업운 "); out.println("길가다가 돈 주울 수 있을지도...?!! 금전운 60%"); break;
+        case "4": out.println("\n 4번의 금전운 "); out.println("당장 로또를 사러가세요! 금전운 100%"); break;
+        case "5": out.println("\n 5번의 애정운 "); out.println("나쁘지않아요!! 금전운 70%"); break;
         default:
           out.println("잘못된 번호를 입력하셨습니다..");
           out.flush();
